b661a3698de8439bd751bf2553c21c56ee84d8ee
Duong Thien Chi <thienchidtc@gmail.com>
2024-10-12 15:16:04 +0700

    Add project files.

---
+++ BC_Market/Factory/Factory.cs
@@ -0,0 +1,14 @@
+﻿using BC_Market.BUS;
+using BC_Market.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BC_Market.Factory
+{
+    internal class Factory
+    {
+    }
+}
